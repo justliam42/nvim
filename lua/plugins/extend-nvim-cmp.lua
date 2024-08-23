85b9130bd5f4489df7a777f39ea6e3ee960df546
Liam Otten <liam.otten@cognex.com>
2024-08-23 10:30:51 -0500

    init commit

---
+++ lua/plugins/extend-nvim-cmp.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = vim.tbl_extend(
+        "force",
+        opts.mapping,
+        {
+          ["<Right>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = function(fallback)
+            cmp.abort()
+            fallback()
+          end
+        }
+      )
+    end,
+  },
+}
