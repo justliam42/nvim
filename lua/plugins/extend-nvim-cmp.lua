9e2756b2aaccbc726145c79f7b0819cf68df2bac
Liam Otten <liam.otten@cognex.com>
2025-04-30 16:28:12 -0500

    Update things so that it is better or something

--- lua/plugins/extend-nvim-cmp.lua
+++ lua/plugins/extend-nvim-cmp.lua
@@ -1,19 +1,55 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
       local cmp = require("cmp")
-      opts.mapping = vim.tbl_extend(
-        "force",
-        opts.mapping,
-        {
-          ["<Right>"] = cmp.mapping.confirm({ select = true }),
-          ["<CR>"] = function(fallback)
-            cmp.abort()
-            fallback()
-          end
-        }
-      )
+      local luasnip = require("luasnip")
+
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_get_current_line():sub(col, col):match("%s") == nil
+      end
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
     end,
   },
 }
